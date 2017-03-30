71c268553eb13fc4be77e70369ca4bca98172cb2
Sam Lewis <sam.vr.lewis@gmail.com>
2017-03-30 15:41:54 +1000

    make functions out public key generating and b58, make more explicit

--- generate_wallet.py
+++ generate_wallet.py
@@ -2,6 +2,7 @@ from ecdsa import SigningKey, SECP256k1
 import random
 import struct
 import hashlib
+import requests
 
 # 58 character alphabet used
 alphabet = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
@@ -11,6 +12,7 @@ def base58_encode(version, payload):
     Gets a Base58Check string
     See https://en.bitcoin.it/wiki/Base58Check_encoding
     """
+    version = bytes.fromhex(version)
     payload_enc = version + payload + hashlib.sha256(hashlib.sha256(version + payload).digest()).digest()[:4]
     result = int.from_bytes(payload_enc, byteorder="big")
 
@@ -24,23 +26,24 @@ def base58_encode(version, payload):
 
     return padding*"1" + "".join(encoded)[::-1]
 
+def get_public_address(private_address):
+    # https://en.bitcoin.it/wiki/Protocol_documentation
+    key_int = int(private_address, 16) 
 
-# https://en.bitcoin.it/wiki/Protocol_documentation
-key_int = int("18E14A7B6A307F426A94F8114701E7C8E774E7F9A47E2C2035DB29A206321725", 16) 
+    key = struct.pack(">Q", (key_int >> 192) & 0xFFFFFFFFFFFFFFFF)
+    key += struct.pack(">Q", (key_int >> 128) & 0xFFFFFFFFFFFFFFFF)
+    key += struct.pack(">Q", (key_int >> 64) & 0xFFFFFFFFFFFFFFFF)
+    key += struct.pack(">Q", key_int & 0xFFFFFFFFFFFFFFFF)
 
-key = struct.pack(">Q", (key_int >> 192) & 0xFFFFFFFFFFFFFFFF)
-key += struct.pack(">Q", (key_int >> 128) & 0xFFFFFFFFFFFFFFFF)
-key += struct.pack(">Q", (key_int >> 64) & 0xFFFFFFFFFFFFFFFF)
-key += struct.pack(">Q", key_int & 0xFFFFFFFFFFFFFFFF)
+    public_key = SigningKey.from_string(key, curve=SECP256k1).verifying_key.to_string()
+    public_key = b"\04" + public_key
+    
+    hashed = hashlib.sha256(public_key).digest()
 
-public_key = SigningKey.from_string(key, curve=SECP256k1).verifying_key.to_string()
-public_key = b"\04" + public_key
+    h = hashlib.new('ripemd160') #uses openssl implementation
+    h.update(hashed)
+    ripehashed = h.digest()
 
-hashed = hashlib.sha256(public_key).digest()
+    return ripehashed
 
-h = hashlib.new('ripemd160') #uses openssl implementation
-h.update(hashed)
-
-ripehashed = h.digest()
-
-print(base58_encode(b'\00', ripehashed))
+print(base58_encode("00", get_public_address("0000123")))
